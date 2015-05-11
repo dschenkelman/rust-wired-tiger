594af2596d4e2acd256905a8fcc84aba1df6e2ac
dschenkelman <damian.schenkelman@gmail.com>
2015-05-11 02:53:36 -0300

    Initial usable lib

--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,36 @@
 extern crate libc;
 
 use libc::{c_int, c_char};
-use wiredtiger_def::{WT_CONNECTION,WT_SESSION,WT_CURSOR,
-  wiredtiger_open,wiredtiger_strerror};
 use std::ffi::{CString,CStr};
 use std::ptr;
 use std::mem;
 use std::{i32,str};
+use wiredtiger::{KeyValuePair};
 
 mod wiredtiger_def;
 mod wiredtiger;
 
-unsafe fn string_from_ptr(ptr: *mut c_char) -> String {
-  let slice = CStr::from_ptr(ptr);
-  str::from_utf8(slice.to_bytes()).unwrap().to_string()
-}
-
-fn get_error(code: i32) -> String {
-  let slice = unsafe {
-    CStr::from_ptr(wiredtiger_strerror(code))
-  };
-
-  str::from_utf8(slice.to_bytes()).unwrap().to_string()
-}
-
-fn c_str(val: &str) -> CString{
-  CString::new(val).unwrap()
-}
-
 fn main() {
-  let action = c_str("create");
-  let mut ret: c_int;
-  // let home = CString::new("create").unwrap();
-  unsafe {
-    let mut connection: *mut WT_CONNECTION = mem::uninitialized();
-    ret = wiredtiger_open(ptr::null(),
-      ptr::null_mut(),
-      action.as_ptr(),
-      &mut connection);
-    if ret != 0{
-      println!("{:?}", get_error(ret));
-    }
-
-    let mut session: *mut WT_SESSION = mem::uninitialized();
-    ret = (*connection).open_session.unwrap()(connection,
-      ptr::null_mut(), ptr::null_mut(), &mut session);
-    if ret != 0{
-      println!("{:?}", get_error(ret));
-    }
-
-    ret = (*session).create.unwrap()(session,
-      c_str("table:access").as_ptr(),
-      c_str("key_format=S,value_format=S").as_ptr());
-    if ret != 0{
-      println!("{:?}", get_error(ret));
-    }
-
-    let mut cursor: *mut WT_CURSOR = mem::uninitialized();
-    ret = (*session).open_cursor.unwrap()(session, c_str("table:access").as_ptr(),
-      ptr::null_mut(),
-      ptr::null_mut(),
-      &mut cursor);
-
-    if ret != 0{
-      println!("{:?}", get_error(ret));
-    }
-
-    let key = c_str("key 1");
-    let val = c_str("value 1");
-
-    (*cursor).set_key.unwrap()(cursor, key.as_ptr());
-    (*cursor).set_value.unwrap()(cursor, val.as_ptr());
-    ret = (*cursor).insert.unwrap()(cursor);
-
-    if ret != 0{
-      println!("{:?}", get_error(ret));
-    }
-
-    ret = (*cursor).reset.unwrap()(cursor);
-
-    if ret != 0{
-      println!("{:?}", get_error(ret));
-    }
-
-    let key: *mut c_char = mem::uninitialized();
-    let value: *mut c_char = mem::uninitialized();
-
-    while (*cursor).next.unwrap()(cursor) == 0 {
-      (*cursor).get_key.unwrap()(cursor, &key);
-      (*cursor).get_value.unwrap()(cursor, &value);
-
-      let key_string = string_from_ptr(key);
-      let value_string = string_from_ptr(value);
-
-      println!("Record {}:{}", key_string, value_string)
-    }
-
-    ret = (*connection).close.unwrap()(connection, ptr::null_mut());
-
-    if ret != 0{
-      println!("{:?}", get_error(ret));
-    }
+  match wiredtiger::open() {
+    Ok(mut connection) => {
+      connection.open_session()
+        .and_then(|mut session|{
+          match session.create_table("users"){
+            Ok(_) => {
+              session.open_cursor("users").and_then(|mut cursor| {
+                try!(cursor.insert_pair("1", "John Doe"));
+                try!(cursor.insert_pair("2", "Jane Doe"));
+                try!(cursor.reset());
+                for el in cursor.filter(|kvp| kvp.value.contains("a")) {
+                  println!("Record {}:{}", el.key, el.value);
+                }
+                Ok(())
+              })
+            },
+            e => e
+          }
+        });
+    },
+    Err(m) => println!("{:?}", m)
   }
-
-  println!("Hello, world!");
 }
--- src/wiredtiger.rs
+++ src/wiredtiger.rs
@@ -43,13 +43,236 @@ pub struct Connection {
   wt_con: *mut WT_CONNECTION
 }
 
+pub struct Session {
+  wt_session: *mut WT_SESSION
+}
+
+pub struct Cursor {
+  wt_cursor: *mut WT_CURSOR
+}
+
+pub struct KeyValuePair {
+  pub key: String,
+  pub value: String
+}
+
 impl Drop for Connection {
   fn drop(&mut self) {
     unsafe{
       match (*self.wt_con).close {
-        Some(f) => { f(self.wt_con, ptr::null_mut()); }
+        Some(close) => { close(self.wt_con, ptr::null_mut()); }
         None => ()
       };
+    }
+  }
+}
+
+impl Connection {
+  pub fn open_session(&mut self) -> Result<Session, String>{
+    unsafe{
+      match (*self.wt_con).open_session {
+        Some(open_session) => {
+          let mut session: *mut WT_SESSION = mem::uninitialized();
+          let ret = open_session(self.wt_con, ptr::null_mut(),
+            ptr::null_mut(), &mut session);
+
+          if ret != 0 {
+            return Err(get_error(ret));
+          }
+
+          Ok(Session{
+            wt_session: session
+          })
+        },
+        None => Err("Failed to get open_session".to_string())
+      }
+    }
+  }
+}
+
+impl Cursor {
+  fn set_key(&mut self, key: &CString) -> Result<(), String>{
+    unsafe {
+      match(*self.wt_cursor).set_key {
+        Some(set_key) => {
+          set_key(self.wt_cursor, key.as_ptr());
+          Ok(())
+        }
+        None => Err("Failed to get set_key".to_string())
+      }
+    }
+  }
+
+  fn set_value(&mut self, value: &CString) -> Result<(), String>{
+    unsafe {
+      match(*self.wt_cursor).set_value {
+        Some(set_value) => {
+          set_value(self.wt_cursor, value.as_ptr());
+          Ok(())
+        }
+        None => Err("Failed to get set_value".to_string())
+      }
+    }
+  }
+
+  fn insert(&mut self) -> Result<(), String>{
+    unsafe {
+      match(*self.wt_cursor).insert {
+        Some(insert) => {
+          let ret = insert(self.wt_cursor);
+
+          if ret != 0 {
+            return Err(get_error(ret));
+          }
+
+          Ok(())
+        }
+        None => Err("Failed to get insert".to_string())
+      }
+    }
+  }
+
+  pub fn insert_pair(&mut self, key: &str, value: &str) -> Result<(), String>{
+    let k = c_str(key);
+    let v = c_str(value);
+    try!(self.set_key(&k));
+    try!(self.set_value(&v));
+    try!(self.insert());
+    Ok(())
+  }
+
+  pub fn reset(&mut self) -> Result<(), String>{
+    unsafe {
+      match(*self.wt_cursor).reset {
+        Some(reset) => {
+          let ret = reset(self.wt_cursor);
+
+          if ret != 0 {
+            return Err(get_error(ret));
+          }
+
+          Ok(())
+        }
+        None => Err("Failed to get reset".to_string())
+      }
+    }
+  }
+
+  pub fn next(&mut self) -> Result<(), String>{
+    unsafe {
+      match(*self.wt_cursor).next {
+        Some(next) => {
+          let ret = next(self.wt_cursor);
+
+          if ret != 0 {
+            return Err(get_error(ret));
+          }
+
+          Ok(())
+        }
+        None => Err("Failed to get next".to_string())
+      }
+    }
+  }
+
+  pub fn get_key(&mut self) -> Result<String, String>{
+    unsafe {
+      match(*self.wt_cursor).get_key {
+        Some(get_key) => {
+          let key: *mut c_char = mem::uninitialized();
+          let ret = get_key(self.wt_cursor, &key);
+
+          if ret != 0 {
+            return Err(get_error(ret));
+          }
+
+          Ok(string_from_ptr(key))
+        }
+        None => Err("Failed to get get_key".to_string())
+      }
+    }
+  }
+
+  pub fn get_value(&mut self) -> Result<String, String>{
+    unsafe {
+      match(*self.wt_cursor).get_value {
+        Some(get_value) => {
+          let value: *mut c_char = mem::uninitialized();
+          let ret = get_value(self.wt_cursor, &value);
+
+          if ret != 0 {
+            return Err(get_error(ret));
+          }
+
+          Ok(string_from_ptr(value))
+        }
+        None => Err("Failed to get get_value".to_string())
+      }
+    }
+  }
+}
+
+impl Session {
+  pub fn create_table(&mut self, name: &str) -> Result<(), String> {
+    unsafe {
+      match(*self.wt_session).create {
+        Some(create) => {
+          let full_command = &format!("table:{0}", name);
+          let ret = create(self.wt_session,
+            c_str(full_command).as_ptr(),
+            c_str("key_format=S,value_format=S").as_ptr());
+
+          if ret != 0 {
+            return Err(get_error(ret));
+          }
+
+          Ok(())
+        }
+        None => Err("Failed to get create".to_string())
+      }
+    }
+  }
+
+  pub fn open_cursor(&mut self, table_name: &str) -> Result<Cursor, String>{
+    unsafe {
+      match(*self.wt_session).open_cursor {
+        Some(open_cursor) => {
+          let mut cursor: *mut WT_CURSOR = mem::uninitialized();
+          let cursor_uid = &format!("table:{0}", table_name);
+          let ret = open_cursor(self.wt_session,
+            c_str(cursor_uid).as_ptr(),
+            ptr::null_mut(),
+            ptr::null_mut(),
+            &mut cursor);
+
+          if ret != 0 {
+            return Err(get_error(ret));
+          }
+
+          Ok(Cursor{
+            wt_cursor: cursor
+          })
+        }
+        None => Err("Failed to get open_cursor".to_string())
+      }
+    }
+  }
+}
+
+impl Iterator for Cursor {
+  type Item = KeyValuePair;
+  fn next(&mut self) -> Option<KeyValuePair> {
+    match self.next() {
+      Ok(_) => {
+        match (self.get_key(), self.get_value()){
+          (Ok(key), Ok(value)) => Some(KeyValuePair{
+            key: key,
+            value: value
+          }),
+          _ => None
+        }
+      }
+      Err(_) => None
     }
   }
 }
