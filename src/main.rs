408191b0cd631512520b68c564bba0f19403837f
dschenkelman <damian.schenkelman@gmail.com>
2015-05-11 02:56:18 -0300

    Simplified sample

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,17 @@ mod wiredtiger;
 fn main() {
   match wiredtiger::open() {
     Ok(mut connection) => {
-      connection.open_session()
-        .and_then(|mut session|{
-          match session.create_table("users"){
-            Ok(_) => {
-              session.open_cursor("users").and_then(|mut cursor| {
-                try!(cursor.insert_pair("1", "John Doe"));
-                try!(cursor.insert_pair("2", "Jane Doe"));
-                try!(cursor.reset());
-                for el in cursor.filter(|kvp| kvp.value.contains("a")) {
-                  println!("Record {}:{}", el.key, el.value);
-                }
-                Ok(())
-              })
-            },
-            e => e
-          }
-        });
+      connection.open_session().and_then(|mut session|{
+        try!(session.create_table("users"));
+        let mut cursor = try!(session.open_cursor("users"));
+        try!(cursor.insert_pair("1", "John Doe"));
+        try!(cursor.insert_pair("2", "Jane Doe"));
+        try!(cursor.reset());
+        for el in cursor.filter(|kvp| kvp.value.contains("a")) {
+          println!("Record {}:{}", el.key, el.value);
+        }
+        Ok(())
+      });
     },
     Err(m) => println!("{:?}", m)
   }
